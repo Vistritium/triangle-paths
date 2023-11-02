80c53ecae60c94b18000330176e8a22360dc0776
Maciej Nowicki <MaciejStefanNowicki@gmail.com>
2023-11-02 10:12:11 +0100

    Added simple input parsing and output priting

--- src/main/scala/trianglepaths/Main.scala
+++ src/main/scala/trianglepaths/Main.scala
@@ -1,8 +1,32 @@
 package trianglepaths
 
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.{Framing, Sink, StreamConverters}
+import akka.stream.{ActorMaterializer, Materializer}
+import akka.util.ByteString
+
+import scala.concurrent.Await
+import scala.io.StdIn
+import scala.concurrent.duration._
+
 object Main extends App {
 
-  println("hi")
+  def readInput(): Seq[Seq[Int]] = {
+    var nextLine = StdIn.readLine()
+    var lines = Seq(nextLine)
+    while (nextLine.nonEmpty) {
+      nextLine = StdIn.readLine()
+      lines = lines :+ nextLine
+    }
+
+    lines.filter(_.nonEmpty).map(_.split(" ").map(_.toInt))
+  }
+
+
+  val input = readInput()
+  println(s"input is \n${input}")
+  val result = Solve.solve(input)
+  println(s"Minimal path is: ${result.mkString(" + ")} = ${result.sum}")
 
 
 }
---
+++ src/main/scala/trianglepaths/Solve.scala
@@ -0,0 +1,9 @@
+package trianglepaths
+
+object Solve {
+
+  def solve(triangle: Seq[Seq[Int]]): Seq[Int] = {
+    Seq(1, 2, 3)
+  }
+
+}
