ae9115f365b0dadaa82775a7c8da3c793aded9fd
Maciej Nowicki <MaciejStefanNowicki@gmail.com>
2023-11-02 10:54:52 +0100

    Maybe working solution, but no memorized path

--- src/main/scala/trianglepaths/Solve.scala
+++ src/main/scala/trianglepaths/Solve.scala
@@ -1,9 +1,31 @@
 package trianglepaths
 
+import scala.collection.mutable
+
 object Solve {
 
   def solve(triangle: Seq[Seq[Int]]): Seq[Int] = {
-    Seq(1, 2, 3)
+
+    def iter(aggr: List[Int], remaining: List[List[Int]]): List[Int] = {
+      if (remaining.isEmpty) aggr
+      else if (aggr.isEmpty) {
+        iter(remaining.head, remaining.tail)
+      } else {
+        val current :: nextRemaining = remaining
+
+        val nextAggr = current.zipWithIndex.map { case (element, i) =>
+          val leftNode = aggr(i)
+          val rightNode = aggr(i + 1)
+          element + Math.min(leftNode, rightNode)
+        }
+
+        iter(nextAggr, nextRemaining)
+
+      }
+    }
+
+    iter(List.empty, triangle.map(_.toList).reverse.toList)
+
   }
 
 }
